75cebaa37b6a1ca4115a8b6a1ac63199c60d5672
Otto Link <otto.link.bv@gmail.com>
2025-10-15 22:43:21 +0200

    Remove debug code

--- examples/ex_hemisphere_field/ex_hemisphere_field.cpp
+++ examples/ex_hemisphere_field/ex_hemisphere_field.cpp
@@ -5,7 +5,6 @@ int main(void)
   hmap::gpu::init_opencl();
 
   hmap::Vec2<int> shape = {256, 256};
-  shape = {1024, 1024};
   hmap::Vec2<float> kw = {16.f, 16.f};
   int               seed = 0;
 
@@ -14,8 +13,6 @@ int main(void)
 
   hmap::Array z2 = hmap::gpu::hemisphere_field_fbm(shape, kw, seed);
   hmap::remap(z2);
-
-  z2.dump();
 
   hmap::export_banner_png("ex_hemisphere_field.png",
                           {z1, z2},
