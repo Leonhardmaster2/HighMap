6b91a92ced9926d10bfef164041576958b7b4d24
Otto Link <otto.link.bv@gmail.com>
2025-12-18 13:27:21 +0100

    Update thermal_flatten example

--- examples/ex_thermal_flatten/ex_thermal_flatten.cpp
+++ examples/ex_thermal_flatten/ex_thermal_flatten.cpp
@@ -9,7 +9,7 @@ int main(void)
   hmap::Array z = hmap::noise_fbm(hmap::NoiseType::PERLIN, shape, res, seed);
   auto        z0 = z;
 
-  hmap::thermal_flatten(z, 8.f / shape.x, 10);
+  hmap::thermal_flatten(z, 4.f / shape.x, 10);
 
   hmap::export_banner_png("ex_thermal_flatten.png",
                           {z0, z},
