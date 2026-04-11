5c54dd1374c4916844d150ff944ca1705029df47
Otto Link <otto.link.bv@gmail.com>
2026-04-11 22:31:56 +0200

    Format sources

--- HighMap/src/local_metrics/local_metrics_wrapper.cpp
+++ HighMap/src/local_metrics/local_metrics_wrapper.cpp
@@ -10,30 +10,54 @@ namespace hmap::gpu
 
 Array local_metrics(const Array &array, int ir, LocalMetrics metric)
 {
+  // clang-format off
   switch (metric)
   {
   case LocalMetrics::LM_LOCAL_ASPECT_VARIANCE:
     return gpu::local_aspect_variance(array, ir);
-  case LocalMetrics::LM_LOCAL_MAX: return gpu::local_max(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_MAX:
+    return gpu::local_max(array, ir);
+    //
   case LocalMetrics::LM_LOCAL_MEDIAN_DEVIATION:
     return gpu::local_median_deviation(array, ir);
-  case LocalMetrics::LM_LOCAL_MIN: return gpu::local_min(array, ir);
-  case LocalMetrics::LM_LOCAL_RELIEF: return gpu::local_relief(array, ir);
-  case LocalMetrics::LM_LOCAL_VARIANCE: return gpu::local_variance(array, ir);
-  case LocalMetrics::LM_LOCAL_MEAN: return gpu::local_mean(array, ir);
-  case LocalMetrics::LM_LOCAL_SKEWNESS: return gpu::local_skewness(array, ir);
-  case LocalMetrics::LM_LOCAL_Z_SCORE: return gpu::local_z_score(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_MIN:
+    return gpu::local_min(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_RELIEF:
+    return gpu::local_relief(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_VARIANCE:
+    return gpu::local_variance(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_MEAN:
+    return gpu::local_mean(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_SKEWNESS:
+    return gpu::local_skewness(array, ir);
+    //
+  case LocalMetrics::LM_LOCAL_Z_SCORE:
+    return gpu::local_z_score(array, ir);
+    //
   case LocalMetrics::LM_TOPOGRAPHIC_POSITION_INDEX:
     return gpu::topographic_position_index(array, ir);
+    //
   case LocalMetrics::LM_RELATIVE_ELEVATION:
     return gpu::relative_elevation(array, ir);
-  case LocalMetrics::LM_RUGGEDNESS: return gpu::ruggedness(array, ir);
+    //
+  case LocalMetrics::LM_RUGGEDNESS:
+    return gpu::ruggedness(array, ir);
+    //
   case LocalMetrics::LM_RUGOSITY_CONCAVE:
     return gpu::rugosity(array, ir, /* convex */ false);
+    //
   case LocalMetrics::LM_RUGOSITY_CONVEX:
     return gpu::rugosity(array, ir, /* convex */ true);
+    //
   default: throw std::runtime_error("unknown LocalMetrics in local_metrics");
   }
+  // clang-format on
 }
 
 } // namespace hmap::gpu
