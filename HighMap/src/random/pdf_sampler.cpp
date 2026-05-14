3a5dc8628a21f1d8d3743410f65dbb376d5f669d
Otto Link <otto.link.bv@gmail.com>
2026-05-14 19:23:21 +0200

    Add PdfSampler

---
+++ HighMap/src/random/pdf_sampler.cpp
@@ -0,0 +1,58 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software. */
+#include <algorithm>
+#include <cstdint>
+#include <numeric>
+#include <random>
+#include <vector>
+
+#include "highmap/random.hpp"
+
+namespace hmap
+{
+
+PdfSampler::PdfSampler(const std::vector<float> &pdf, uint32_t seed)
+    : generator(seed)
+{
+  float sum = std::accumulate(pdf.begin(), pdf.end(), 0.f);
+
+  cdf.resize(pdf.size());
+
+  float cumulative = 0.f;
+
+  for (size_t i = 0; i < pdf.size(); ++i)
+  {
+    cumulative += pdf[i] / sum;
+    cdf[i] = cumulative;
+  }
+}
+
+float PdfSampler::sample()
+{
+  float u = distribution(generator);
+
+  auto it = std::lower_bound(cdf.begin(), cdf.end(), u);
+
+  size_t index = std::distance(cdf.begin(), it);
+
+  // Uniform random offset inside the bin
+  float local = distribution(generator);
+
+  return static_cast<float>((static_cast<float>(index) + local) /
+                            static_cast<float>(cdf.size()));
+}
+
+std::vector<float> PdfSampler::sample(size_t nb_samples)
+{
+  std::vector<float> samples;
+
+  samples.reserve(nb_samples);
+
+  for (size_t i = 0; i < nb_samples; ++i)
+    samples.push_back(sample());
+
+  return samples;
+}
+
+} // namespace hmap
