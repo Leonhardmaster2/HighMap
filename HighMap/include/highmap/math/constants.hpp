44334973eef7e48ba794c26326236b67a4a35f75
Otto Link <otto.link.bv@gmail.com>
2026-05-22 09:19:34 +0200

    Fix cmath constat issues

--- HighMap/include/highmap/math/constants.hpp
+++ HighMap/include/highmap/math/constants.hpp
@@ -9,23 +9,6 @@
  */
 #pragma once
 
-// force defition, for Windows port
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-#ifndef M_PI_2
-#define M_PI_2 1.57079632679489661923
-#endif
-
-#ifndef M_SQRT2
-#define M_SQRT2 1.41421356237309504880
-#endif
-
-#ifndef M_SQRT1_2
-#define M_SQRT1_2 0.707106781186547524401
-#endif
-
 namespace hmap
 {
 
