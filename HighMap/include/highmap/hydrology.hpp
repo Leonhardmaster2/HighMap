359aa756d679f1566e41ca8492790a5ccd3ad18b
Otto Link <otto.link.bv@gmail.com>
2026-05-28 21:17:18 +0200

    Add top level hydrology header

---
+++ HighMap/include/highmap/hydrology.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+   Public License. The full license is in the file LICENSE, distributed with
+   this software. */
+
+/**
+ * @file hydrology.hpp
+ * @author Otto Link (otto.link.bv@gmail.com)
+ * @copyright Copyright (c) 2023
+ */
+#pragma once
+
+#include "highmap/hydrology/drainage_basin_cell_based.hpp"
+#include "highmap/hydrology/drainage_basin.hpp"
+#include "highmap/hydrology/hydrology.hpp"
+
