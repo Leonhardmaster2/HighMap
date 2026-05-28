f04b9b519bf477f160e61d7ecaf43a574016f711
Otto Link <otto.link.bv@gmail.com>
2026-05-28 20:56:45 +0200

    Add top level geometry header

---
+++ HighMap/include/highmap/geometry.hpp
@@ -0,0 +1,18 @@
+/* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
+   Public License. The full license is in the file LICENSE, distributed with
+   this software. */
+
+/**
+ * @file geometry.hpp
+ * @author Otto Link (otto.link.bv@gmail.com)
+ * @copyright Copyright (c) 2023
+ */
+#pragma once
+
+#include "highmap/geometry/cloud.hpp"
+#include "highmap/geometry/graph.hpp"
+#include "highmap/geometry/grids.hpp"
+#include "highmap/geometry/kd_tree.hpp"
+#include "highmap/geometry/path.hpp"
+#include "highmap/geometry/point.hpp"
+#include "highmap/geometry/point_sampling.hpp"
