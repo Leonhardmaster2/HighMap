af8bd1fb5bf77b627060775f6b26b100af282b58
otto-link <otto-link@users.noreply.github.com>
2026-06-23 17:16:58 +0000

    deploy: ba739a9020d91a8f93b0224fe4a1e283fe07b67b

--- strata_8cpp.js
+++ strata_8cpp.js
@@ -5,6 +5,6 @@ var strata_8cpp =
     [ "strata_cells", "strata_8cpp.html#a740cd2ba7c58126ef27bfcddcb6f00b8", null ],
     [ "strata_cells_fbm", "strata_8cpp.html#a5b29dc1e29512877770fd208cd40b971", null ],
     [ "strata_cells_fbm", "strata_8cpp.html#ab8f6820b9bd796b3283e763f4d872a51", null ],
-    [ "strata_terrace", "strata_8cpp.html#a6c5ef6ef70ac1b265d556ce9ce4ad042", null ],
-    [ "strata_terrace", "strata_8cpp.html#a288ef6bf36b0f0bb7085ca04a6b0a40e", null ]
+    [ "strata_terrace", "strata_8cpp.html#a7bfb87c4fc0f9f4fe3a043cb18e7f8fa", null ],
+    [ "strata_terrace", "strata_8cpp.html#a05cc60e4654cd2f0dbb5021bf7602bb9", null ]
 ];
